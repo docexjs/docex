935ce8f0b48f6f5a623419ff185c84355a676d5b
shu1gin <dmitry.shulgin@inbox.ru>
2022-03-22 01:52:05 +0700

    feat: resolve conflict

--- lib/docex.ts
+++ lib/docex.ts
@@ -1,19 +1,27 @@
 import * as path from 'path';
 import * as fs from 'fs';
 import * as SwaggerParser from 'swagger-parser';
+import { OpenAPIV3 } from 'openapi-types';
+import * as Excel from 'exceljs';
 import * as Cache from './cache';
 import {
-    InvalidMiddlewareArgs,
+    INVALID_MIDDLEWARE_ARGS_MSG,
+    InvalidOpenapiFile,
     MissingRequiredOption,
     NotFoundOpenapiFile
 } from './errors';
-import { DocexOptions, MiddlewareArgs } from './interfaces';
+import {
+    ConstructDocumentParams,
+    DocexOptions,
+    MiddlewareArgs,
+    Type,
+    Data
+} from './interfaces';
+
+const OPENAPI_CACHE_KEY = 'openapi';
+const cache = Cache.getInstance();
 
 const unzipMiddlewareArgs = (args): MiddlewareArgs => {
-    if (!Array.isArray(args)) {
-        throw new InvalidMiddlewareArgs();
-    }
-
     if (args.length === 2) {
         return {
             req: args[0].req,
@@ -29,6 +37,86 @@ const unzipMiddlewareArgs = (args): MiddlewareArgs => {
     }
 }
 
+const parseOpenapi = async (openapiPath: string): Promise<OpenAPI.Document> => {
+    try {
+        return await SwaggerParser.parse(openapiPath);
+    } catch (e) {
+        throw new InvalidOpenapiFile();
+    }
+}
+
+const cacheOpenapi = async (openapiPath: string) => {
+    try {
+        const parsedOpenapi = await parseOpenapi(openapiPath);
+        cache.set(OPENAPI_CACHE_KEY, parsedOpenapi);
+    } catch (e) {
+        throw e;
+    }
+
+}
+
+const getOpenapiSchema = (url, method = 'get'): OpenAPIV3.SchemaObject => {
+    const openapi: OpenAPIV3.Document = cache.get(OPENAPI_CACHE_KEY);
+
+    const path = openapi.paths[url][method];
+    const response = path.responses[200];
+    const content = response.content['application/json'];
+    const schema = content.schema;
+
+    let ref
+
+    if (schema?.type === 'array') {
+        ref = schema.items.$ref;
+    }
+
+    ref = schema.$ref;
+
+    if (!ref) {
+        //
+    }
+
+    const paths = ref.split('/');
+    const schemaName = paths[paths.length - 1];
+    const schemaData = openapi.components.schemas[schemaName];
+
+    if (!schemaData) {
+        //
+    }
+
+    return schemaData as OpenAPIV3.SchemaObject;
+}
+
+const collectRows = () => {
+
+}
+
+const constructPDF = async (data: Data | Data[], schema: OpenAPIV3.SchemaObject, type: Type) => {
+
+}
+
+const constructXLSX = async (data: Data | Data[], schema: OpenAPIV3.SchemaObject, type: Type) => {
+    const workbook = new Excel.Workbook();
+    const worksheet = workbook.addWorksheet();
+
+    let rows;
+
+    if (type === 'table') {
+
+    } else if (type === 'list') {
+
+    }
+}
+
+const constructDocument = async (params: ConstructDocumentParams) => {
+    const schema = getOpenapiSchema(params.url, params.method);
+
+    if (params.ext === 'pdf') {
+        await constructPDF(params.data, schema, params.type);
+    } else if (params.ext === 'xlsx') {
+        await constructXLSX(params.data, schema, params.type);
+    }
+}
+
 const docex = (options: DocexOptions) => {
     if (!options?.openapiPath) {
         throw new MissingRequiredOption('openapiPath');
@@ -38,18 +126,43 @@ const docex = (options: DocexOptions) => {
     const openapiPath = path.join(rootPath, options.openapiPath);
 
     const isExistsOpenapiFile = fs.existsSync(openapiPath);
+
     if (!isExistsOpenapiFile) {
         throw new NotFoundOpenapiFile();
     }
 
     return async (...args) => {
+        if (!Array.isArray(args)) {
+            console.error(INVALID_MIDDLEWARE_ARGS_MSG);
+            process.exit(1);
+        }
+
         const {
             req,
             res,
             next
         }: MiddlewareArgs = unzipMiddlewareArgs(args);
 
-        return await next();
+        const data = await next();
+
+        try {
+            if (!cache.has(OPENAPI_CACHE_KEY)) {
+                await cacheOpenapi(openapiPath);
+            }
+
+            const params: ConstructDocumentParams = {
+                data,
+                openapi: cache.get(OPENAPI_CACHE_KEY),
+                url: req.url,
+                method: req.method.toLowerCase(),
+                ext: req?.body?.ext ?? 'json',
+                type: req?.body?.type ?? 'table'
+            }
+
+            return await constructDocument(params);
+        } catch (e) {
+            next(e);
+        }
     }
 }
 

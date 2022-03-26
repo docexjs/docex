486b2f08d4f8a035409a3d937e25ca266a904d84
shu1gin <dmitry.shulgin@inbox.ru>
2022-03-26 19:56:08 +0700

    feat: divide functional into files

--- lib/docex.ts
+++ lib/docex.ts
@@ -1,15 +1,12 @@
 import * as path from 'path';
 import * as fs from 'fs';
-import * as SwaggerParser from 'swagger-parser';
 import {
-    OpenAPI,
     OpenAPIV3,
 } from 'openapi-types';
 import * as Excel from 'exceljs';
 import * as Cache from './cache';
 import {
     INVALID_MIDDLEWARE_ARGS_MSG,
-    InvalidOpenapiFile,
     MissingRequiredOption,
     NotFoundOpenapiFile
 } from './errors';
@@ -20,10 +17,16 @@ import {
     Type,
     Data
 } from './interfaces';
-
-const OPENAPI_CACHE_KEY = 'openapi';
-const LIST_TYPE_NAME = 'list';
-const TABLE_TYPE_NAME = 'table';
+import {
+    cacheOpenapi,
+    getOpenapiSchema
+} from './openapi';
+import {
+    isObj,
+    wrapDataToArrayData,
+    wrapSchemaProperties
+} from './utils';
+import { CONFIG } from './config';
 
 const cache = Cache.getInstance();
 
@@ -43,59 +46,11 @@ const unzipMiddlewareArgs = (args): MiddlewareArgs => {
     }
 }
 
-const parseOpenapi = async (openapiPath: string): Promise<OpenAPI.Document> => {
-    try {
-        return await SwaggerParser.parse(openapiPath);
-    } catch (e) {
-        throw new InvalidOpenapiFile();
-    }
-}
-
-const cacheOpenapi = async (openapiPath: string) => {
-    try {
-        const parsedOpenapi = await parseOpenapi(openapiPath);
-        cache.set(OPENAPI_CACHE_KEY, parsedOpenapi);
-    } catch (e) {
-        throw e;
-    }
-}
-
-const getOpenapiSchema = (url, method = 'get'): OpenAPIV3.SchemaObject => {
-    const openapi: OpenAPIV3.Document = cache.get(OPENAPI_CACHE_KEY);
-
-    const path = openapi.paths[url][method];
-    const response = path.responses[200];
-    const content = response.content['application/json'];
-    const schema = content.schema;
-
-    let ref
-
-    if (schema?.type === 'array') {
-        ref = schema.items.$ref;
-    }
-
-    ref = schema.$ref;
-
-    if (!ref) {
-        //
-    }
-
-    const paths = ref.split('/');
-    const schemaName = paths[paths.length - 1];
-    const schemaData = openapi.components.schemas[schemaName];
-
-    if (!schemaData) {
-        //
-    }
-
-    return schemaData as OpenAPIV3.SchemaObject;
-}
-
-const flatNestedProperty = (elementData, properties, type) => {
-    let flat = [];
+const flatNestedValue = (elementData, properties, type) => {
+    let flatValue = [];
 
     const addPaddingToFlat = () => {
-        flat.push(['', '']);
+        flatValue.push(['', '']);
     }
 
     let arrayData = wrapDataToArrayData(elementData);
@@ -113,40 +68,24 @@ const flatNestedProperty = (elementData, properties, type) => {
             if (Array.isArray(element[propertyKey])) {
                 let innerProperties = properties[propertyKey].properties;
 
-                if (type === LIST_TYPE_NAME) {
+                if (type === CONFIG.LIST_TYPE_NAME) {
                     addPaddingToFlat();
-                    flat.push([propertyValue.title, '']);
+                    flatValue.push([propertyValue.title, '']);
                     addPaddingToFlat();
                 }
 
-                flatNestedProperty(element[propertyKey], innerProperties, type);
-
+                flatNestedValue(element[propertyKey], innerProperties, type);
             }
 
-            flat.push([propertyValue.title, element[propertyKey]]);
+            flatValue.push([propertyValue.title, element[propertyKey]]);
         }
 
-        if (type === LIST_TYPE_NAME) {
+        if (type === CONFIG.LIST_TYPE_NAME) {
             addPaddingToFlat();
         }
     }
 
-    return flat;
-}
-
-const wrapDataToArrayData = (data: Data | Data[]): Data[] => {
-    return Array.isArray(data) ? data : [data];
-}
-
-const wrapSchemaProperties = (properties): Array<{
-    key: string,
-    value: OpenAPIV3.SchemaObject
-}> => {
-    return Object.entries(properties).map(([key, value]) => ({ key, value }));
-}
-
-const isObj = (o) => {
-    return typeof o === 'object' && !Array.isArray(o) && o !== null;
+    return flatValue;
 }
 
 const collectRows = (data: Data | Data[], schema: OpenAPIV3.SchemaObject, type: Type): any[] => {
@@ -169,7 +108,7 @@ const collectRows = (data: Data | Data[], schema: OpenAPIV3.SchemaObject, type: 
             let propertyKey = schemaProperty.key;
             let propertyValue = schemaProperty.value;
 
-            if (propertyKey === 'number' && type !== LIST_TYPE_NAME) {
+            if (propertyKey === 'number' && type !== CONFIG.LIST_TYPE_NAME) {
                 row.push(++dIndex);
                 continue;
             }
@@ -178,35 +117,35 @@ const collectRows = (data: Data | Data[], schema: OpenAPIV3.SchemaObject, type: 
             let value = element[propertyKey];
 
             if (Array.isArray(value) || isObj(value)) {
-                const flat = flatNestedProperty(value, propertyValue.properties, type);
+                const flatValue = flatNestedValue(value, propertyValue.properties, type);
 
-                if (type === LIST_TYPE_NAME) {
+                if (type === CONFIG.LIST_TYPE_NAME) {
                     addPaddingToRows();
                     rows.push([header, '']);
                     addPaddingToRows();
-                    rows.push(...flat);
+                    rows.push(...flatValue);
                     addPaddingToRows();
 
                     continue;
                 }
 
-                let str = flat.map(([key, value]) => `${key}: ${value}`).join('\n')
+                let str = flatValue.map(([key, value]) => `${key}: ${value}`).join('\n')
                 row.push(str);
 
                 continue;
             }
-            if (type === LIST_TYPE_NAME) {
+            if (type === CONFIG.LIST_TYPE_NAME) {
                 rows.push([header, value]);
                 continue;
             }
             row.push(value);
         }
 
-        if (!row.length && type === LIST_TYPE_NAME) {
+        if (!row.length && type === CONFIG.LIST_TYPE_NAME) {
             continue;
         }
 
-        if (type !== LIST_TYPE_NAME) {
+        if (type !== CONFIG.LIST_TYPE_NAME) {
             rows.push(row);
         }
     }
@@ -242,9 +181,9 @@ const constructXLSX = async (data: Data | Data[], schema: OpenAPIV3.SchemaObject
     const workbook = new Excel.Workbook();
     const worksheet = workbook.addWorksheet();
 
-    if (type === TABLE_TYPE_NAME) {
+    if (type === CONFIG.TABLE_TYPE_NAME) {
         worksheet.columns = getTableColumns(schema.properties);
-    } else if (type === LIST_TYPE_NAME) {
+    } else if (type === CONFIG.LIST_TYPE_NAME) {
         worksheet.columns = getListColumns();
     }
 
@@ -299,13 +238,13 @@ const docex = (options: DocexOptions) => {
         const data = await next();
 
         try {
-            if (!cache.has(OPENAPI_CACHE_KEY)) {
+            if (!cache.has(CONFIG.OPENAPI_CACHE_KEY)) {
                 await cacheOpenapi(openapiPath);
             }
 
             const params: ConstructDocumentParams = {
                 data,
-                openapi: cache.get(OPENAPI_CACHE_KEY),
+                openapi: cache.get(CONFIG.OPENAPI_CACHE_KEY),
                 url: req.url,
                 method: req.method.toLowerCase(),
                 ext: req?.body?.ext ?? 'xlsx',

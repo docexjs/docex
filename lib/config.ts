c25d1601efe618dd001809eb375f46988d7b2048
shu1gin <dmitry.shulgin@inbox.ru>
2022-03-27 23:32:33 +0700

    feat: refactor constructPDF, getTableDocDefinitionPdf

--- lib/config.ts
+++ lib/config.ts
@@ -1,5 +1,7 @@
 export const CONFIG = {
     OPENAPI_CACHE_KEY: 'openapi',
     LIST_TYPE_NAME: 'list',
-    TABLE_TYPE_NAME: 'table'
+    TABLE_TYPE_NAME: 'table',
+    PDF_EXT_NAME: 'pdf',
+    XLSX_EXT_NAME: 'xlsx'
 }
